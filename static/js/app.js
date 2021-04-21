502f7fe8f42edaaa821bb45be102942b8fde39ca
beaujeffrey03 <75648641+beaujeffrey03@users.noreply.github.com>
2021-04-21 12:57:05 -0500

    Made code for bar graph.

--- static/js/app.js
+++ static/js/app.js
@@ -1,9 +1,43 @@
 // Some of the code here comes from office hours with Dom.
 
-console.log('app.js loaded');
+// console.log('app.js loaded');
 
+
+// draw graphs
 function DrawBargraph(sampleId) {
     console.log(`DrawBargraph(${sampleId})`);
+
+    d3.json('data/samples.json').then(data => {
+        // console.log(data);
+
+        var samples = data.samples;
+        var resultArray = samples.filter(s => s.id == sampleId);
+        var result = resultArray[0]
+
+        var otu_ids = result.otu_ids;
+        var otu_labels = result.otu_labels;
+        var sample_values = result.sample_values;
+
+        yticks = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
+
+        var barData = {
+            x: sample_values.slice(0, 10).reverse(),
+            y: yticks,
+            type: 'bar',
+            text: otu_labels.slice(0, 10).reverse(),
+            orientation: 'h'
+        }
+
+        var barArray = [barData]
+
+        var barLayout = {
+            title: `Top 10 Bacteria Cultures Found (Subject ${sampleId})`,
+            margin: {t: 30, l: 150}
+        }
+
+        Plotly.newPlot('bar', barArray, barLayout)
+
+    });
 }
 
 function DrawBubblechart(sampleId) {
@@ -14,6 +48,8 @@ function ShowMetadata(sampleId) {
     console.log(`ShowMetadata(${sampleId})`);
 }
 
+
+// make function for event handler
 function optionChanged(newSampleId) {
     console.log(`User selected ${newSampleId}`);
 
