c3f1d77dcd21c1d0e600b9c5664155bc45c1b2ea
beaujeffrey03 <75648641+beaujeffrey03@users.noreply.github.com>
2021-04-22 08:22:06 -0500

    Built bubble chart.

--- static/js/app.js
+++ static/js/app.js
@@ -2,13 +2,11 @@
 
 // console.log('app.js loaded');
 
-
-// draw graphs
+// draw bar graphs
 function DrawBargraph(sampleId) {
-    console.log(`DrawBargraph(${sampleId})`);
+    // console.log(`DrawBargraph(${sampleId})`);
 
     d3.json('data/samples.json').then(data => {
-        // console.log(data);
 
         var samples = data.samples;
         var resultArray = samples.filter(s => s.id == sampleId);
@@ -41,11 +39,66 @@ function DrawBargraph(sampleId) {
 }
 
 function DrawBubblechart(sampleId) {
-    console.log(`DrawBubblechart(${sampleId})`);
+    // console.log(`DrawBubblechart(${sampleId})`);
+
+    d3.json('data/samples.json').then(data => {
+
+        var samples = data.samples;
+        var resultArray = samples.filter(s => s.id == sampleId);
+        var result = resultArray[0]
+
+        var otu_ids = result.otu_ids;
+        var otu_labels = result.otu_labels;
+        var sample_values = result.sample_values;
+
+        var bubbleData = {
+            x: otu_ids.slice(0, 500),
+            y: sample_values.slice(0, 50),
+            mode: 'markers',
+            marker: {
+                size: sample_values,
+                color: otu_ids
+                },
+            text: otu_labels.slice(0, 10)
+        }
+
+        var barArray = [bubbleData]
+
+        var barLayout = {
+            margin: {t: 30, l: 50},
+            xaxis: {
+                title: {
+                text: 'OTU ID',
+                font: {
+                    // family: 'Courier New, monospace',
+                    size: 18,
+                    color: '#7f7f7f'
+                }
+                },
+            },
+            yaxis: {
+                title: {
+                text: 'Sample Value',
+                font: {
+                    // family: 'Courier New, monospace',
+                    size: 18,
+                    color: '#7f7f7f'
+                }
+                }
+            }
+        }
+
+    Plotly.newPlot('bubble', barArray, barLayout)
+
+    });
+    
 }
 
 function ShowMetadata(sampleId) {
     console.log(`ShowMetadata(${sampleId})`);
+
+
+        
 }
 
 
@@ -67,7 +120,7 @@ function InitDashboard() {
 
     d3.json('data/samples.json').then(data => {
 
-        // console.log(data);
+        console.log(data);
 
         var sampleNames = data.names;
 
